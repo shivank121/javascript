0dd988b4ccc4e7912c3519b4ef17fb5c4fb2eb49
shivank121 <shivanksingh634@gmail.com>
2023-11-06 11:31:03 +0530

    about type of memory

---
+++ basic_js/6_memory.js
@@ -0,0 +1,13 @@
+// ****************************MEMORY****************************
+
+// Types of Memory:
+//   i- Stack memory  -> all "Preimitive" data types store in Stack memory
+//   ii- Heap memory  -> all "Non-primitive" data types store in Heap memory
+
+
+let name = "shivank singh";
+let anotherName = name
+anotherName = "chai aur code"
+
+console.log(name)
+console.log(anotherName)
