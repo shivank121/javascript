4223f735937e0b53dac403061772c9863572f0cd
shivank121 <shivanksingh634@gmail.com>
2023-11-06 11:27:56 +0530

    about data types

---
+++ basic_js/3_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all the code of JS as a newer version
+// alert("hello bro how are you")  // We are using node JS not browser 
+
+
+
+// PRIMITIVE Data Types
+let name = "shivank"  // string dataType
+let num = 1234   //integer dataType
+let value = true  //boolean  store (true or false)
+let state = null  // null -> this is standalone value (Representaion of emty value)
+let will ;  // undifined -> 
+            // symbol (Use for uniqueness)
+
+
+
+console.log(typeof name)  // "typeof" is keyword that used to check type of variables
+
+
+//  -->  NOTE  
+
+// Types of data types
+//    => (1) -> Primitive data type   
+//                 =>[total 7 types] 
+//                     => (i-String, ii-Number, iii-Boolean, iv-Null, v-Undefine, vi-Symbol, vii-BigInt)
+
+//    => (2) -> Non-primitive or Reference data type 
+//                 =>[total 3 types]
+//                     => (i-Array, ii-Objects, iii-Functions)
