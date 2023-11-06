d4378fd79157aa0d72010543392e0d7f0103f363
shivank121 <shivanksingh634@gmail.com>
2023-11-06 11:30:11 +0530

    comparison operators uses

---
+++ basic_js/5_comparision.js
@@ -0,0 +1,26 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+
+console.log("2" > 1);  // this is bad habit make sure both shoud be same data type
+
+
+//TRICKY LOGIC
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)  // By default Null convert in 0
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+
+// === use for Strict check 
+console.log(2 === 2)  // === check dataType and also check value
+console.log("2" === 2)
+
