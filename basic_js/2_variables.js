feebc105d32ac41bbdc6e4dd52e1af68f5ee3900
shivank121 <shivanksingh634@gmail.com>
2023-11-06 11:27:34 +0530

    about variables

---
+++ basic_js/2_variables.js
@@ -0,0 +1,23 @@
+const accountId = 764972;
+let accountEmail = "shivanksingh@.com";
+var accountPassword= "jaunpur";
+
+let accountState;  // Undifined variable
+
+
+// accountId = 1234;   // here try to modify the values of var 
+console.log(accountId)
+accountEmail = "vivekYadav";
+console.log(accountEmail)
+
+accountPassword="hiiii"  // update value of accountPasswoerd variable
+
+console.log([accountId, accountEmail, accountPassword]) // print three variale at same time on by one using "Table"
+
+
+
+
+// Note
+//    -> we can not able to modify "const"
+//    -> we are able to modify "let"
+//    -> we are able to modify "var"
